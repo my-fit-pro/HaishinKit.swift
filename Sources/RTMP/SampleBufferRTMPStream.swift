7173bda37fec53f8aeb4daf48fd6bf01a141b3af
Zach Simone <zach@harbourcity.co>
2023-09-18 11:39:13 +1000

    Update RTMPStream subclass to account for recent API changes.

--- Sources/RTMP/SampleBufferRTMPStream.swift
+++ Sources/RTMP/SampleBufferRTMPStream.swift
@@ -13,21 +13,27 @@ open class SampleBufferRTMPStream: RTMPStream {
     public var includeVideoMetaData: Bool = true
     public var includeAudioMetaData: Bool = true
 
-    override open func createMetaData() -> ASObject {
+    override open func makeMetaData() -> ASObject {
         var metadata: ASObject = [:]
         #if os(iOS) || os(macOS)
         if includeVideoMetaData {
             metadata["width"] = mixer.videoIO.codec.settings.videoSize.width
             metadata["height"] = mixer.videoIO.codec.settings.videoSize.height
             metadata["framerate"] = mixer.videoIO.frameRate
-            if mixer.videoIO.codec.settings.format == .h264 {
+            switch mixer.videoIO.codec.settings.format {
+            case .h264:
                 metadata["videocodecid"] = FLVVideoCodec.avc.rawValue
+            case .hevc:
+                metadata["videocodecid"] = FLVVideoFourCC.hevc.rawValue
             }
             metadata["videodatarate"] = mixer.videoIO.codec.settings.bitRate / 1000
         }
         if includeAudioMetaData {
             metadata["audiocodecid"] = FLVAudioCodec.aac.rawValue
             metadata["audiodatarate"] = mixer.audioIO.codec.settings.bitRate / 1000
+            if let sampleRate = mixer.audioIO.codec.inSourceFormat?.mSampleRate {
+                metadata["audiosamplerate"] = sampleRate
+            }
         }
         #endif
         return metadata
